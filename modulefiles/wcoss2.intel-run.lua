305ab0e5d9e4926486e3a28f401210e198ed62c9
David Huber <david.huber@noaa.gov>
2025-01-03 16:55:18 +0000

    Use spack-stack on WCOSS2

--- modulefiles/wcoss2.intel-run.lua
+++ modulefiles/wcoss2.intel-run.lua
@@ -1,21 +1,22 @@
 help([[
 ]])
 
-local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
+prepend_path("MODULEPATH", "/apps/ops/test/spack-stack-1.6.0-nco/envs/nco-intel-19.1.3.304/install/modulefiles/Core")
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "19.1.3.304"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.9"
+
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.13"
 local prod_envir_ver=os.getenv("prod_envir_ver") or "2.0.6"
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
-local wgrib2_ver=os.getenv("wgrib2_ver") or "2.0.8"
-
-load(pathJoin("intel", intel_ver))
-load(pathJoin("prod_util", prod_util_ver))
-load(pathJoin("prod_envir", prod_envir_ver))
-
-load(pathJoin("netcdf", netcdf_ver))
-load(pathJoin("wgrib2", wgrib2_ver))
+load(pathJoin("stack-intel", intel_ver))
+load(pathJoin("stack-cray-mpich", cray_mpich_ver))
 
 prepend_path("MODULEPATH", "/apps/test/lmodules/core/")
 load ("GrADS/2.2.2")
+load(pathJoin("prod_util", prod_util_ver))
+load(pathJoin("prod_envir", prod_envir_ver))
+
+load("common-run")
 
 whatis("Description: GSI Monitoring run-time environment on wcoss2")
--- modulefiles/wcoss2.intel.lua
+++ modulefiles/wcoss2.intel.lua
@@ -1,26 +1,16 @@
 help([[
 ]])
 
-local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
-local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-local craype_ver=os.getenv("craype_ver") or "2.7.8"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
-local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
+prepend_path("MODULEPATH", "/apps/ops/test/spack-stack-1.6.0-nco/envs/nco-intel-19.1.3.304/install/modulefiles/Core")
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
-local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-local ncdiag_ver=os.getenv("ncdiag_ver") or "1.0.0"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "19.1.3.304"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.9"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
-load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
-load(pathJoin("intel", intel_ver))
-load(pathJoin("craype", craype_ver))
-load(pathJoin("cray-mpich", cray_mpich_ver))
+load(pathJoin("stack-intel", intel_ver))
+load(pathJoin("stack-cray-mpich", cray_mpich_ver))
 load(pathJoin("cmake", cmake_ver))
 
-load(pathJoin("netcdf", netcdf_ver))
-load(pathJoin("bacio", bacio_ver))
-load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("ncdiag", ncdiag_ver))
+load("common")
 
 whatis("Description: GSI Monitoring environment on WCOSS2")
